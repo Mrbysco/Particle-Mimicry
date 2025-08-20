f782997f8e25a72dfb9dbeee2998b65615e12b6d
Mrbysco <Mrbysco@users.noreply.github.com>
2025-08-20 20:06:59 +0200

    Remove unused imports

--- forge/src/main/java/com/mrbysco/particlemimicry/mixin/ParticleEmitterBlockEntityMixin.java
+++ forge/src/main/java/com/mrbysco/particlemimicry/mixin/ParticleEmitterBlockEntityMixin.java
@@ -1,9 +1,6 @@
 package com.mrbysco.particlemimicry.mixin;
 
-import com.mrbysco.particlemimicry.Constants;
 import com.mrbysco.particlemimicry.blocks.entity.ParticleEmitterBlockEntity;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.registries.VanillaRegistries;
@@ -14,9 +11,6 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ParticleEmitterBlockEntity.class)
 public class ParticleEmitterBlockEntityMixin extends BlockEntity {
