1b5cd2382a0cd77d6e1b19556ac9065c42f43a2b
Mrbysco <Mrbysco@users.noreply.github.com>
2026-04-26 16:30:17 +0200

    Update MimicryBlockTagProvider.java

--- fabric/src/main/java/com/mrbysco/particlemimicry/datagen/data/MimicryBlockTagProvider.java
+++ fabric/src/main/java/com/mrbysco/particlemimicry/datagen/data/MimicryBlockTagProvider.java
@@ -4,16 +4,14 @@ import com.mrbysco.particlemimicry.registration.MimicryRegistry;
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagsProvider;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.BlockTags;
-import net.minecraft.world.level.block.Block;
 
 import java.util.concurrent.CompletableFuture;
 
-public class MimicryBlockTagProvider extends FabricTagsProvider.FabricIntrinsicHolderTagsProvider<Block> {
+public class MimicryBlockTagProvider extends FabricTagsProvider.BlockTagsProvider {
 
 	public MimicryBlockTagProvider(FabricPackOutput output, CompletableFuture<HolderLookup.Provider> registriesFuture) {
-		super(output, Registries.BLOCK, registriesFuture, block -> block.builtInRegistryHolder().key());
+		super(output, registriesFuture);
 	}
 
 	@Override
